c2a682eb260193da750691fd0f59296ff0d0790e
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-07-17 01:21:35 +0900

    Title: 1시간 뒤 시간 출력, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240717/1시간 뒤 시간 출력/print-one-hour-later.java
+++ 240717/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        // sc.useDelimeter(":");
-        // int a = sc.nextInt();
-        // int b = sc.nextInt();
-        // System.out.println((a+1) + ":" + b);
-        String s = sc.next();
-        String[] strArr = s.split(":");
-        System.out.println(Integer.parseInt(strArr[0])+1 + ":" + strArr[1]);
+        sc.useDelimiter(":");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println((a+1) + ":" + b);
+        // String s = sc.next();
+        // String[] strArr = s.split(":");
+        // System.out.println(Integer.parseInt(strArr[0])+1 + ":" + strArr[1]);
     }
 }
