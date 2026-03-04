f7b4405e58b10511716eafdacf996b8e438bf837
GureumKim <90162048+GureumKim@users.noreply.github.com>
2026-03-04 21:41:16 +0900

    Title: 단어 출력, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

--- 260304/단어 출력/print-word.java
+++ 260304/단어 출력/print-word.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        System.out.Println("Hello");
+        System.out.println("Hello");
     }
 }
